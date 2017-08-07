0be86d780262652ec700d6961a227b3a12d96e9c
tang\Y481 <tom20170805@sina.com>
2017-08-07 10:59:25 +0800

    再次尝试！！！一定成功

---
+++ tests/tests/tests.cpp
@@ -0,0 +1,15 @@
+// tests.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include"iomanip"
+#include"iostream"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"hello"<<endl;
+	system("pause");
+	return 0;
+}
+
